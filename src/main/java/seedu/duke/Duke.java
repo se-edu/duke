b23c61a176f064731e17f2673ee6f70bc7bdc705
Jeffry Lum <22460123+j-lum@users.noreply.github.com>
2019-07-16 12:23:32 +0800

    Add main class
    As there was no code at all, students might not arrive at an appropriate
    project structure required for later steps.
    
    Let's add a simple main class to preserve the project structure and to
    give students a starting point.

---
+++ src/main/java/seedu/duke/Duke.java
@@ -0,0 +1,12 @@
+package seedu.duke;
+
+public class Duke {
+    public static void main(String[] args) {
+        String logo = " ____        _        \n" +
+                      "|  _ \\ _   _| | _____ \n" +
+                      "| | | | | | | |/ / _ \\\n" +
+                      "| |_| | |_| |   <  __/\n" +
+                      "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
