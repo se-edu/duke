cf9b73323f8b8c3c727a54fbbeaf38d1deec70d6
Jeffry Lum <22460123+j-lum@users.noreply.github.com>
2019-08-06 16:14:46 +0800

    Add support for semi-automated text testing
    Implement an `exit` command for testing purposes. Otherwise, the
    testing script will have to rely on user input to end successfully and
    defeat the purpose of automated testing.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +10,20 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        // Sample implementation of an exit command.
+        while(true) {
+            try {
+                String userInput = in.readLine();
+                if (userInput.trim().equals("exit")) {
+                    System.exit(0);
+                }
+                System.out.println(userInput);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
