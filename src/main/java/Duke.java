81d836ce2235dced08d20dd519a3a046baffb4b5
Elysia Tan <>
2020-01-18 11:13:12 +0800

    Level-3: Mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,14 +11,24 @@ public class Duke {
         Scanner scanner = new Scanner(System.in);
 
         String input = scanner.nextLine();
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int numTasks = 0;
 
         while (!input.equals("bye")) {
+            String[] inputs = input.split(" ");
+
             if (input.equals("list")) {
                 printList(tasks, numTasks);
+            } else if (inputs[0].equals("done")) {
+                int completedTask = Integer.valueOf(inputs[1]);
+
+                tasks[completedTask - 1].markAsDone();
+
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(" [" + tasks[completedTask - 1].getStatusIcon() + "] "
+                        + tasks[completedTask - 1].getDescription());
             } else {
-                tasks[numTasks] = input;
+                tasks[numTasks] = new Task(input);
                 numTasks++;
 
                 System.out.println("added: " + input);
@@ -35,9 +45,56 @@ public class Duke {
      * @param tasks List of tasks that are saved
      * @param numTasks Number of tasks in the list
      */
-    private static void printList(String[] tasks, int numTasks) {
+    private static void printList(Task[] tasks, int numTasks) {
+        System.out.println("Here are the tasks in your list:");
+
         for (int i = 1; i < numTasks + 1; i++) {
-            System.out.println(i + ". " + tasks[i - 1]);
+            System.out.println(i + ".[" + tasks[i - 1].getStatusIcon() + "] "
+                    + tasks[i - 1].getDescription());
         }
+    }
+}
+
+/**
+ * Each object of class Task represents a task to be saved
+ */
+class Task {
+    private String description;
+    private boolean isDone;
+
+    /**
+     * Class constructor
+     * @param description Task description
+     */
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    /**
+     * Returns symbol representing status of task
+     * @return If task is done, return tick symbol, else return X symbol
+     */
+    public String getStatusIcon() {
+        if (isDone) {
+            return ("\u2713");
+        } else {
+            return ("\u2718");
+        }
+    }
+
+    /**
+     * Returns description of task
+     * @return description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Mark task as done
+     */
+    public void markAsDone() {
+        isDone = true;
     }
 }
