6876e117f0c1ea64b365f4cba89a4f3d8b1b39d9
waaaflesss <50788391+waaaflesss@users.noreply.github.com>
2020-01-20 23:46:06 +0800

    Duke.java: add text UI test files, A-TextUiTesting

--- src/main/java/DukeFormatting.java
+++ src/main/java/DukeFormatting.java
@@ -1,12 +1,12 @@
 class DukeFormatting {
     private static final String LOGO =
-            "                  M\"\"\"\"\"\"\"`YM\"\"MMMMM\"\"MP\"\"\"\"\"\"`MM \n" +
-            "                  M  mmmm.  M  MMMMM  M  mmmmm..M \n" +
-            "          .d8888b.M  MMMMM  M  MMMMM  M.      `YM \n" +
-            "          88'  `88M  MMMMM  M  MMMMM  MMMMMMM.  M \n" +
-            "          88.  .88M  MMMMM  M  `MMM'  M. .MMM'  M \n" +
-            "          `88888P'M  MMMMM  Mb       dMb.     .dM \n" +
-            "                  MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM \n";
+            "                  M\"\"\"\"\"\"\"`YM\"\"MMMMM\"\"MP\"\"\"\"\"\"`MM\n" +
+            "                  M  mmmm.  M  MMMMM  M  mmmmm..M\n" +
+            "          .d8888b.M  MMMMM  M  MMMMM  M.      `YM\n" +
+            "          88'  `88M  MMMMM  M  MMMMM  MMMMMMM.  M\n" +
+            "          88.  .88M  MMMMM  M  `MMM'  M. .MMM'  M\n" +
+            "          `88888P'M  MMMMM  Mb       dMb.     .dM\n" +
+            "                  MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\n";
 
     static final String DIVIDER = "____________________________________________________________\n";
     static final String HELLO = DukeFormatting.DIVIDER + DukeFormatting.LOGO
