71c7a9f5a50af73cd06e2f93d73c4b251f404a2c
Zhu-Ruicong <traviszhuruicong@gmail.com>
2020-01-20 14:29:56 +0800

    add Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,8 @@
+import java.util.Scanner;
+
 public class Duke {
+
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +10,29 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        introduce();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String next = scanner.nextLine();
+            if (next.equals("bye")) {
+                break;
+            } else {
+                respond(next);
+            }
+        }
+        respond("Bye. Hope to see you again soon!");
+    }
+
+    public static void respond(String in) {
+
+        String line = "     _____________________________________________";
+        String output = line + "\n" + "     " + in + "\n" + line;
+        System.out.println(output);
+    }
+
+    public static void introduce() {
+        String intro_message = "Hello! I'm Duke" + "\n" + "     " + "What can I do for you?";
+        respond(intro_message);
     }
 }
