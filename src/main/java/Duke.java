f8bab6cc63b2afa3bcc79f4ceb19916dba9f9c1d
Shaun Goh <43333891+SHAUNGOH97@users.noreply.github.com>
2020-01-21 01:27:58 +0800

    redo level -4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -20,82 +20,40 @@ public class Duke {
         while (true) {
             String input = sc.nextLine();
             if (input.equals("bye")) {
-                //Leaves Duke
-                System.out.println ("Cya next time!");
+                System.out.println("Cya next time!");
                 break;
-            } else if (input.equals ("list")) {
-                //Open up list of Task
-                System.out.println ("Here are the tasks in your list: ");
+            } else if (input.equals("list")) {
                 int num = lst.size();
-                for (int i = 1; i <= num; i++) {
-                    Task task = lst.get(i - 1);
-                    System.out.println(i + ". " + task);
+                for (int i = 0; i < num; i++) {
+                    System.out.println((i + 1) + ". " + lst.get(i));
                 }
             } else if (input.contains ("done")) {
-                //Mark task as done
                 try {
-                    int taskNum = (sc.nextInt()) - 1;
-                    sc.nextLine();
-                    if (taskNum > lst.size()) {
-                        //If user inputs invalid task number
-                        System.out.println("No such task!\n");
-                    } else {
-                        //User marks undone task as done
-                        Task completedTask = lst.get(taskNum);
-                        if (!completedTask.isItDone()) {
+                    int taskNum = Integer.parseInt(input.substring(5));
+                    if (taskNum <= lst.size()) {
+                        Task completedTask = lst.get(taskNum - 1);
+                        if (completedTask.getStatus().equals("Done")) {
+                            System.out.println ("You have already completed this task!");
+                        } else {
                             completedTask.markAsDone();
                             pendingTask--;
-                        } else {
-                            //Indicating a task that has already been done
-                            System.out.println ("This task has already been done!");
                         }
                         if (pendingTask == 0) {
-                            //Indicate to user there are no more pending task
-                            System.out.println("0 task remaining! You are free:)\n");
+                            System.out.println ("Yay! You have no more task remaining!");
                         } else {
-                            System.out.println ("You have " + pendingTask + " task remaining!");
+                            System.out.println ("You have " + pendingTask + " tasks remaining!");
                         }
+                    } else {
+                        System.out.println ("There is no such task!");
                     }
                 } catch (Exception e){
-                    //In the case user inputs a non-int data type
-                    sc.nextLine();
-                    System.out.println ("Please type in a task number:(");
+                    System.out.println ("Sorry, I dont understand you request!");
                 }
             } else {
-                //User adding a new task
-                switch (input.toLowerCase()) {
-                    case "todo":
-                        System.out.println("Okay! Whats your todo task?");
-                        Todo task1 = new Todo (sc.nextLine());
-                        lst.add(task1);
-                        pendingTask++;
-                        System.out.println ("Got it! I'm adding this to your list:\n" + task1);
-                        System.out.println ("Now you have " + pendingTask + " task remaining.");
-                        break;
-                    case "deadline":
-                        System.out.println("Okay! What is your deadline task?");
-                        String deadlineTask = sc.nextLine();
-                        System.out.println("Alright. When is the deadline?");
-                        Deadline task2 = new Deadline (deadlineTask, sc.nextLine());
-                        lst.add(task2);
-                        pendingTask++;
-                        System.out.println ("Got it! I'm adding this to your list:\n" + task2);
-                        System.out.println ("Now you have " + pendingTask + " task in your list.");
-                        break;
-                    case "event":
-                        System.out.println("Okay! What event is coming up?");
-                        String event = sc.nextLine();
-                        System.out.println ("Alright! What time is the event?");
-                        String eventTime = sc.nextLine();
-                        Event task3 = new Event (event, eventTime);
-                        lst.add(task3);
-                        pendingTask++;
-                        System.out.println ("Got it! I'm adding this to your list:\n " + task3);
-                        System.out.println ("Now you have " + pendingTask + " task remaining.");
-                        break;
-                    default:
-                        System.out.println ("Invalid Command");
-                }
+                Task task = new Task (input);
+                lst.add (task);
+                pendingTask++;
+                System.out.println ("added: " + task);
             }
         }
     }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,38 +1,23 @@
 public class Task {
     protected String description;
-    protected boolean isDone;
-    protected String taskSymbol = "X";
-    public String checkMark = "Done";
-    public String cross     = "Not Done";
-    //public char checkMark = '\u2713';
-    //public char cross     = '\u2718';
-    //Cannot print unicode
+    protected String status = "Not Done";
 
     public Task (String description) {
         this.description = description;
-        this.isDone = false;
-    }
-
-    public boolean isItDone() {
-        return this.isDone;
-    }
-
-    public String getStatus() {
-        return (isDone ? checkMark : cross);
     }
 
     public void markAsDone() {
-        this.isDone = true;
-        System.out.println ("Nice! You have marked this task as done:\n["
-            + checkMark + "] " + this.description);
+        this.status = "Done";
+        System.out.println ("Nice! I've marked this task as done:\n"
+                + "[" + this.status + "] " + this.description);
     }
 
-    public String getSymbol() {
-        return this.taskSymbol;
+    public String getStatus() {
+        return this.status;
     }
 
     @Override
     public String toString() {
-        return this.description;
+        return "[" + this.status + "] " + this.description;
     }
 }
