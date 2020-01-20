d329c6d1509537ebb2d1d5bd0a07b4a7cc9e61c4
yapdianhao1011 <yapdhao@gmail.com>
2020-01-21 02:15:49 +0800

    level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -28,19 +28,55 @@ public class Duke {
                 return;
             } else if (input.equals("list")) {
                 for (int i = 0; i < strings.size(); i++) {
-                    System.out.println((i + 1) + "." + strings.get(i).getStatusIcon() + " " + strings.get(i));
+                    System.out.println((i + 1) + "." + strings.get(i));
                 }
             } else if (input.split(" ")[0].equals("done")) {
                 int idx = Integer.parseInt((input.split(" ")[1])) - 1;
                 strings.get(idx).setDone();
                 System.out.println("Nice! I've marked this task as done:");
-                System.out.println(strings.get(idx).getStatusIcon() + " " + strings.get(idx));
-            }
-            else {
+                System.out.println(strings.get(idx));
+            } else if (input.split(" ")[0].equals("todo")) {
+                input = input.substring(input.split(" ")[0].length() + 1, input.length());
                 Task task = new Task(input);
                 strings.add(task);
-                System.out.println("added: " + task);
+                addTask(task);
+            } else if (input.split(" ")[0].equals("deadline")) {
+                input = input.substring(input.split(" ")[0].length() + 1, input.length());
+                String name = input.split(" /by")[0];
+                String time = input.split(" /by")[1];
+                Deadline deadline = new Deadline(name, time);
+                strings.add(deadline);
+                addTask(deadline);
+            } else if (input.split(" ")[0].equals("event")) {
+                input = input.substring(input.split(" ")[0].length() + 1, input.length());
+                String name = input.split(" /at")[0];
+                String time = input.split(" /at")[1];
+                Event event = new Event(name, time);
+                strings.add(event);
+                addTask(event);
+            } else {
+                System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(");
             }
         }
+    }
+
+    static void addDeadline(Deadline deadline) {
+        System.out.println("Got it. I've added this task:");
+        System.out.println(deadline);
+        System.out.println("Now you have " + strings.size()
+                + (strings.size() > 1 ? "tasks" : "task") + " in the list");
+    }
+
+    static void addEvent(Event event) {
+        System.out.println("Got it. I've added this task:");
+        System.out.println(event);
+        System.out.println("Now you have " + strings.size()
+                + (strings.size() > 1 ? "tasks" : "task") + " in the list");
+    }
+    static void addTask(Task task) {
+        System.out.println("Got it. I've added this task:");
+        System.out.println(task);
+        System.out.println("Now you have " + strings.size()
+                + " " + (strings.size() > 1 ? "tasks" : "task") + " in the list");
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -9,14 +9,17 @@ public class Task {
     }
 
     public String getStatusIcon() {
-        return "[" + (isDone ? "\u2713" : "\u2718") + "]";
+        return (isDone ? "\u2713" : "\u2718");
     }
 
     public void setDone() {
         this.isDone = true;
     }
 
+    public String getDescription() {
+        return "[" + getStatusIcon() + "] " + this.description;
+    }
     public String toString() {
-        return this.description;
+        return "[T]" + getDescription();
     }
 }
