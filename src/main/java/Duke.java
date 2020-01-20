e88982f8eae869746a3ec31faef0e15904843e7c
Shaun Goh <43333891+SHAUNGOH97@users.noreply.github.com>
2020-01-20 13:39:06 +0800

    Finished Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
 
@@ -12,17 +13,23 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         String line = "__________________________";
+        ArrayList <String> lst = new ArrayList <String>();
         System.out.println(line + "\nWhat can i do for you?\n" + line);
 
         while (true) {
-            String input = sc.next();
+            String input = sc.nextLine();
             if (input.equals("bye")) {
                 System.out.println("Cya next time!");
                 break;
+            } else if (input.equals("list")) {
+                int num = lst.size();
+                for (int i = 1; i < num; i++) {
+                    System.out.println(i + ". " + lst.get(i));
+                }
             } else {
-                System.out.println(input);
+                lst.add (input);
+                System.out.println ("added: " + input);
             }
         }
     }
-    #Hello this is an edit
 }
