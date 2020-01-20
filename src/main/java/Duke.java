72dd71e2dabe44472ce862a788f47ad2f5e45081
waaaflesss <50788391+waaaflesss@users.noreply.github.com>
2020-01-20 17:18:54 +0800

    add DukeFormatting class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,6 @@
 import java.util.Scanner;
 
 public class Duke {
-    private static final String LOGO =  "        8888888b.           888\n"
-            + "        888  \"Y88b          888\n"
-            + "        888    888          888\n"
-            + "        888    888 888  888 888  888  .d88b.\n"
-            + "        888    888 888  888 888 .88P d8P  Y8b\n"
-            + "        888    888 888  888 888888K  88888888\n"
-            + "        888  .d88P Y88b 888 888 \"88b Y8b.\n"
-            + "        8888888P\"   \"Y88888 888  888  \"Y8888=\n\n";
-    private static final String DIVIDER = "______________________________________________________\n";
-    private static final String LIST = "list\n";
-    private static final String BLAH = "blah\n";
-    private static final String BYE = "Bye. Hope to see you again soon!\n";
 
     private static void parseInput() {
         Scanner sc = new Scanner(System.in);
@@ -22,13 +10,16 @@ public class Duke {
 
             switch (command) {
                 case "list":
-                    System.out.println(DIVIDER + LIST + DIVIDER);
+                    System.out.println(DukeFormatting.DIVIDER +
+                            DukeFormatting.LIST + DukeFormatting.DIVIDER);
                     break;
                 case "blah":
-                    System.out.println(DIVIDER + BLAH + DIVIDER);
+                    System.out.println(DukeFormatting.DIVIDER + DukeFormatting.BLAH
+                            + DukeFormatting.DIVIDER);
                     break;
                 case "bye":
-                    System.out.println(DIVIDER + BYE + DIVIDER);
+                    System.out.println(DukeFormatting.DIVIDER + DukeFormatting.BYE
+                            + DukeFormatting.DIVIDER);
                     break;
             }
 
@@ -39,9 +30,10 @@ public class Duke {
     }
 
     public static void main(String[] args) {
-        System.out.println(DIVIDER + LOGO + "Hello! I'm Duke! " +
+        System.out.println(DukeFormatting.DIVIDER + DukeFormatting.LOGO
+                + "Hello! I'm Duke! " +
                 "\nThese are my commands: list, blah, bye. " +
-                "\nWhat can I do for you?\n" + DIVIDER);
+                "\nWhat can I do for you?\n" + DukeFormatting.DIVIDER);
 
         parseInput();
     }
---
+++ src/main/java/DukeFormatting.java
@@ -0,0 +1,14 @@
+class DukeFormatting {
+    static final String LOGO =  "        8888888b.           888\n"
+            + "        888  \"Y88b          888\n"
+            + "        888    888          888\n"
+            + "        888    888 888  888 888  888  .d88b.\n"
+            + "        888    888 888  888 888 .88P d8P  Y8b\n"
+            + "        888    888 888  888 888888K  88888888\n"
+            + "        888  .d88P Y88b 888 888 \"88b Y8b.\n"
+            + "        8888888P\"   \"Y88888 888  888  \"Y8888=\n\n";
+    static final String DIVIDER = "______________________________________________________\n";
+    static final String LIST = "list\n";
+    static final String BLAH = "blah\n";
+    static final String BYE = "Bye. Hope to see you again soon!\n";
+}
