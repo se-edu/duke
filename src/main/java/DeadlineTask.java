08a61d6f835570b4571e12abb9ead50b5c0986c7
waaaflesss <50788391+waaaflesss@users.noreply.github.com>
2020-01-20 22:00:18 +0800

    DeadlineTask.java: added file, A-Inheritance

---
+++ src/main/java/DeadlineTask.java
@@ -0,0 +1,19 @@
+public class DeadlineTask extends Task {
+    private String deadline;
+
+    DeadlineTask(String description, boolean isDone, String deadline) {
+        super(description, isDone);
+        this.deadline = deadline;
+    }
+
+    @Override
+    DeadlineTask completeTask() {
+        return new DeadlineTask(this.description, true, this.deadline);
+    }
+
+    @Override
+    public String toString() {
+        return "[T] [" + super.getStatusIcon() + "] " + this.description
+                + " (" + this.deadline + ")";
+    }
+}
