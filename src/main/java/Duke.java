d2de7095616711810b89ed9cdd773d26858a1145
Elysia Tan <>
2020-01-21 15:15:10 +0800

    Level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,12 +11,34 @@ public class Duke {
         Scanner scanner = new Scanner(System.in);
 
         String input = scanner.nextLine();
+        String[] tasks = new String[100];
+        int numTasks = 0;
 
         while (!input.equals("bye")) {
-            System.out.println(input);
+            if (input.equals("list")) {
+                printList(tasks, numTasks);
+            } else {
+                tasks[numTasks] = input;
+                numTasks++;
+
+                System.out.println("added: " + input);
+            }
+
             input = scanner.nextLine();
         }
 
         System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    /**
+     * Prints the list of tasks currently saved.
+     *
+     * @param tasks List of tasks that are saved.
+     * @param numTasks Number of tasks in the list.
+     */
+    private static void printList(String[] tasks, int numTasks) {
+        for (int i = 1; i < numTasks + 1; i++) {
+            System.out.println(i + ". " + tasks[i - 1]);
+        }
     }
 }
