180db52eff05e91ad4f8a0194a1092de07ff7b0b
Jia Zheng <ljiazh3ng@gmail.com>
2020-01-22 03:43:23 +0800

    Finish setting up gradle with Checkstyle, Jar and dependencies

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,9 @@
 public class Duke {
+
+    /**
+     * Prints a logo of the name Duke.
+     * @param args Set up main function
+     */
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
