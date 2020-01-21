a0e8d00ccfe5b496b67291863515f05a07f94e6c
zhouxinwei97 <45256284+zhouxinwei97@users.noreply.github.com>
2020-01-21 19:54:59 +0800

    level 1 done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,34 @@
+
+import java.util.Scanner;
+
+/**
+ * For level-1 Greet, Echo, Exit.
+ */
 public class Duke {
+
+    /**
+     * Main method.
+     * @param args arguments
+     */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello! I'm Duke\n"
+                + "     What can I do for you?");
+
+        while (true) {
+            String response = sc.nextLine();
+            if (response.equals("bye")) {
+                System.out.println("     ____________________________________________________________");
+                System.out.println("     Bye. Hope to see you again soon!");
+                System.out.println("     ____________________________________________________________");
+                break;
+            } else {
+                System.out.println("     ____________________________________________________________");
+                System.out.println("     " + response);
+                System.out.println("     ____________________________________________________________");
+            }
+        }
+
     }
 }
