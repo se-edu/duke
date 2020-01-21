3548e43273783cdd3201efd2bd6f5ce770ef664b
dinhnhobao <nhobao2000@gmail.com>
2020-01-21 16:23:51 +0800

    update bot greetings

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,13 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("I am here to help you with anything you need!");
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.nextLine();
+        if (userInput.equals("bye")) {
+            System.out.println("Bye. Hope to see you again soon!");
+        } else {
+            System.out.println(userInput);
+        }
     }
 }
