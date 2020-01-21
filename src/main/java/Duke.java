59c15307f204d6a9b62d01f43031b313f2080784
dinhnhobao <=>
2020-01-21 16:43:06 +0800

    update bot add, list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,12 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Duke {
     public static void main(String[] args) {
+
+        List<String> history = new ArrayList<>();
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -10,11 +15,19 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("I am here to help you with anything you need!");
         Scanner sc = new Scanner(System.in);
-        String userInput = sc.nextLine();
-        if (userInput.equals("bye")) {
-            System.out.println("Bye. Hope to see you again soon!");
-        } else {
-            System.out.println(userInput);
+
+        while (sc.hasNext()) {
+            String userInput = sc.nextLine();
+            if (userInput.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+            } else if (userInput.equals("list")) {
+                for (int i = 0; i < history.size(); i++) {
+                    System.out.println((i+1) + ". " + history.get(i));
+                }
+            } else {
+                System.out.println("added: " + userInput);
+                history.add(userInput);
+            }
         }
     }
 }
