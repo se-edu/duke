c0d7283ba52fafd948e1681b3869cebd1c2938bf
dinhnhobao <e0313562@u.nus.edu>
2020-01-21 17:06:37 +0800

    create Task class with operations

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Duke {
     public static void main(String[] args) {
-        List<String> history = new ArrayList<>();
+        List<Task> history = new ArrayList<>();
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -19,12 +19,18 @@ public class Duke {
             if (userInput.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
             } else if (userInput.equals("list")) {
+                System.out.println("Here are the tasks in your list:\n");
                 for (int i = 0; i < history.size(); i++) {
-                    System.out.println((i+1) + ". " + history.get(i));
+                    System.out.println((i + 1) + "." + history.get(i).toString());
                 }
+            } else if (userInput.startsWith("done")) {
+                int taskNumber = -1 + Integer.parseInt(userInput.split(" ")[1]);
+                history.get(taskNumber).markAsDone();
+                System.out.println("Nice! I've marked this task as done: ");
+                System.out.println(history.get(taskNumber));
             } else {
                 System.out.println("added: " + userInput);
-                history.add(userInput);
+                history.add(new Task(userInput));
             }
         }
     }
