86e3d643925b6e8ab66290e1b5eccd680af98f83
phaebaez <50545929+phaebaez@users.noreply.github.com>
2020-01-23 19:06:39 +0800

    completed level-4 deadline

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -34,7 +34,16 @@ public class Duke {
                 System.out.println("Got it. I've added this task: ");
                 System.out.println("  " + task.toString());
                 System.out.println("Now you have " + (counter) + " tasks in your list.");
-            } else {
+            } else if (nextString.contains("deadline")){
+                String name = nextString.substring(9);
+                String[] temp = name.split(" /by ");
+                Task task = new Deadline(temp[0], temp[1]);
+                tasklist.add(counter, task);
+                counter++;
+                System.out.println("Got it. I've added this task: ");
+                System.out.println("  " + task.toString());
+                System.out.println("Now you have " + (counter) + " tasks in your list.");
+            }else {
                 Task t = new Task(nextString);
                 tasklist.add(counter, t);
                 System.out.println("added: " + nextString);
