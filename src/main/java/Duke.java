75f2428c3f39df0131a8784c3f3592109f59b637
phaebaez <50545929+phaebaez@users.noreply.github.com>
2020-01-23 18:42:03 +0800

    completed level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,7 +5,7 @@ public class Duke {
     private static int counter = 0;
 
     public static void main(String[] args) {
-        ArrayList<String> tasklist = new ArrayList<>();
+        ArrayList<Task> tasklist = new ArrayList<>();
         System.out.println("Hey, I'm Duke");
         System.out.println("What's up");
 
@@ -15,11 +15,20 @@ public class Duke {
             if (nextString.equals("bye")) {
                 System.out.println("Bye.");
             } else if (nextString.equals("list")){
-                for (String s: tasklist) {
-                    System.out.println((tasklist.indexOf(s) + 1) + ". " + s);
+                System.out.println("Here are the tasks in your list: ");
+                for (Task t: tasklist) {
+                    System.out.println((tasklist.indexOf(t) + 1) + ". [" + t.getStatusIcon() + "] " + t.description);
                 }
+            } else if (nextString.contains("done")){
+                String[] temp = nextString.split(" ");
+                int index = Integer.parseInt(temp[1]) - 1;
+                System.out.println("Nice! I've marked this task as done: ");
+                Task current = tasklist.get(index);
+                current.isDone = true;
+                System.out.println("[" + current.getStatusIcon() + "] " + current.description);
             } else {
-                tasklist.add(counter, nextString);
+                Task t = new Task(nextString);
+                tasklist.add(counter, t);
                 System.out.println("added: " + nextString);
                 counter++;
             }
