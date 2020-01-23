8e8ac7d3759444ddba59e46db1c5f412fa3217b1
phaebaez <50545929+phaebaez@users.noreply.github.com>
2020-01-23 18:14:09 +0800

    completed level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,27 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+    private static int counter = 0;
+
     public static void main(String[] args) {
+        ArrayList<String> tasklist = new ArrayList<>();
         System.out.println("Hey, I'm Duke");
         System.out.println("What's up");
 
         Scanner scanner = new Scanner(System.in);
         while(scanner.hasNext()) {
             String nextString = scanner.nextLine();
-            if(nextString.equals("bye")) {
+            if (nextString.equals("bye")) {
                 System.out.println("Bye.");
+            } else if (nextString.equals("list")){
+                for (String s: tasklist) {
+                    System.out.println((tasklist.indexOf(s) + 1) + ". " + s);
+                }
             } else {
-                System.out.println(nextString);
+                tasklist.add(counter, nextString);
+                System.out.println("added: " + nextString);
+                counter++;
             }
         }
     }
