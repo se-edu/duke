1d578d231da010de967705031be9d0323a898acb
teikjun <teikjunhci@gmail.com>
2020-01-23 18:37:18 +0800

    update Duke.java to Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Duke.printGreeting();
+        String input = "";
+        while (!(input = sc.nextLine()).equals("bye")) {
+            Duke.echo(input);
+        }
+        Duke.printExit();
+    }
+
+    public static void echo(String input) {
+        System.out.println(input);
+    }
+
+    public static void printGreeting() {
+        System.out.println("Hello! I'm Duke\n"
+                + "What can I do for you?");
+    }
+
+    public static void printExit() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void printLogo() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
