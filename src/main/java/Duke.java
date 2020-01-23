c0937b552c670577d4506ed28beb34a9052a518e
gyant <e0325592@u.nus.edu>
2020-01-23 18:42:56 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,14 +13,24 @@ public class Duke {
         System.out.println("What can I do for you?");
 
         Scanner sc = new Scanner(System.in);
+
+        String[] list = new String[100];
+        int listCount = 0;
+
         while (true) {
             String userInput = sc.nextLine();
 
             if (userInput.equals("bye")) {
                 System.out.println("Bye! Come back soon!");
                 break;
+            } else if (userInput.equals("list")) {
+                for (int i = 0; i < listCount; i++) {
+                    System.out.println((i+1) + ". " + list[i]);
+                }
             } else {
-                System.out.println(userInput);
+                System.out.println("added: " + userInput);
+                list[listCount] = userInput;
+                listCount++;
             }
         }
     }
