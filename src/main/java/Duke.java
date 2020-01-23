606f72392cf3a7ddea36c5fccd6e64d74bede583
gyant <e0325592@u.nus.edu>
2020-01-23 18:33:13 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,20 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello! I'm Duke!");
+        System.out.println("What can I do for you?");
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String userInput = sc.nextLine();
+
+            if (userInput.equals("bye")) {
+                System.out.println("Bye! Come back soon!");
+                break;
+            } else {
+                System.out.println(userInput);
+            }
+        }
     }
 }
