923c7a377320190f5def7c78736b60869e1e2182
phaebaez <50545929+phaebaez@users.noreply.github.com>
2020-01-23 19:01:32 +0800

    completed level-4 todo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -17,7 +17,7 @@ public class Duke {
             } else if (nextString.equals("list")){
                 System.out.println("Here are the tasks in your list: ");
                 for (Task t: tasklist) {
-                    System.out.println((tasklist.indexOf(t) + 1) + ". [" + t.getStatusIcon() + "] " + t.description);
+                    System.out.println((tasklist.indexOf(t) + 1) + ". " + t.toString());
                 }
             } else if (nextString.contains("done")){
                 String[] temp = nextString.split(" ");
@@ -25,7 +25,15 @@ public class Duke {
                 System.out.println("Nice! I've marked this task as done: ");
                 Task current = tasklist.get(index);
                 current.isDone = true;
-                System.out.println("[" + current.getStatusIcon() + "] " + current.description);
+                System.out.println(current.toString());
+            } else if (nextString.contains("todo")){
+                String name = nextString.substring(5);
+                Task task = new Todo(name);
+                tasklist.add(counter, task);
+                counter++;
+                System.out.println("Got it. I've added this task: ");
+                System.out.println("  " + task.toString());
+                System.out.println("Now you have " + (counter) + " tasks in your list.");
             } else {
                 Task t = new Task(nextString);
                 tasklist.add(counter, t);
