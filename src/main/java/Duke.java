d035d0c11f464a776764d8fdaf4939faeafad77c
gyant <e0325592@u.nus.edu>
2020-01-26 13:27:56 +0800

    Shift code from main method into separate methods to clean main method

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -26,86 +26,100 @@ public class Duke {
                 break;
             } else if (userInput.equals("list")) {
                 // Print out list of all tasks
-                System.out.println("This is your list of tasks");
-                for (int i = 0; i < list.size(); i++) {
-                    Task t = list.get(i);
-                    System.out.println((i+1) + "." + t);
-                }
+                printList(list);
             } else if (userInput.contains("done")) {
                 // Mark one task as complete
-                //int taskNumber =  userInput.charAt(5);
                 try {
-                    if (userInput.equals("done")) throw new DukeException(userInput);
-                    int taskNumber = Character.getNumericValue(userInput.charAt(5));
-                    if (taskNumber > list.size()) throw new DukeException("List size");
-                    Task t = list.get(taskNumber - 1);
-                    t.markAsDone();
-                    System.out.println("This task is marked as completed!");
-                    System.out.println(t);
+                    completeTask(list, userInput);
                 } catch (DukeException e) {
                     e.print();
                 }
             } else if (userInput.contains("delete")) {
                 // Remove task
                 try {
-                    if (userInput.equals("delete")) throw new DukeException(userInput);
-                    int taskNumber = Character.getNumericValue(userInput.charAt(7));
-                    if (taskNumber > list.size()) throw new DukeException("List size");
-                    Task t = list.get(taskNumber - 1);
-                    list.remove(taskNumber - 1);
-                    System.out.println("This task has successfully been deleted!");
-                    System.out.println(t);
-                    System.out.println("You have " + list.size() + " tasks remaining.");
+                    deleteTask(list, userInput);
                 } catch (DukeException e) {
                     e.print();
                 }
             } else {
                 // Add new task
                 try {
-                    String taskDescription = "";
-                    Task t;
+                    addTask(list, userInput);
 
-                    if (userInput.contains("todo")) {
-                        // add Todo task
-                        if (userInput.equals("todo")) throw new DukeException(userInput);
-                        taskDescription = userInput.substring(5); // removes todo word
-                        t = new Todo(taskDescription);
-                    } else if (userInput.contains("deadline")) {
-                        // add Deadline task
-                        if (userInput.equals("deadline")) throw new DukeException(userInput);
-                        taskDescription = userInput.substring((9)); // removes deadline word
-                        int slashIdx = taskDescription.indexOf("/");
-                        if (slashIdx == -1) throw new DukeException("Format");
 
-                        String taskTitle = taskDescription.substring(0, slashIdx);
-                        String deadline = taskDescription.substring(slashIdx + 4);
-                        t = new Deadline(taskTitle, deadline);
-                    } else if (userInput.contains("event")) {
-                        // add Event task
-                        if (userInput.equals("event")) throw new DukeException(userInput);
-                        taskDescription = userInput.substring(6); // removes event word
-                        int slashIdx = taskDescription.indexOf("/");
-                        if (slashIdx == -1) throw new DukeException("Format");
-
-                        String taskTitle = taskDescription.substring(0, slashIdx);
-                        String location = taskDescription.substring(slashIdx + 4);
-                        t = new Event(taskTitle, location);
-                    } else {
-                        t = null;
-                        throw new DukeException("Unrecognised");
-                    }
-
-                    //Task t = new Task(taskDescription);
-                    if (t != null) {
-                        list.add(t);
-                        System.out.println("This task has been added successfully:");
-                        System.out.println(t);
-                        System.out.println("Now you have " + list.size() + " tasks in the list");
-                    }
                 } catch (DukeException e) {
                     e.print();
                 }
             }
+        }
+    }
+
+    private static void addTask(ArrayList<Task> list, String userInput) throws DukeException {
+        Task t;
+        if (userInput.contains("todo")) {
+            // add Todo task
+            if (userInput.equals("todo")) throw new DukeException(userInput);
+            String taskDescription = userInput.substring(5); // removes todo word
+            t = new Todo(taskDescription);
+        } else if (userInput.contains("deadline")) {
+            // add Deadline task
+            if (userInput.equals("deadline")) throw new DukeException(userInput);
+            String taskDescription = userInput.substring((9)); // removes deadline word
+            int slashIdx = taskDescription.indexOf("/");
+            if (slashIdx == -1) throw new DukeException("Format");
+
+            String taskTitle = taskDescription.substring(0, slashIdx);
+            String deadline = taskDescription.substring(slashIdx + 4);
+            t = new Deadline(taskTitle, deadline);
+        } else if (userInput.contains("event")) {
+            // add Event task
+            if (userInput.equals("event")) throw new DukeException(userInput);
+            String taskDescription = userInput.substring(6); // removes event word
+            int slashIdx = taskDescription.indexOf("/");
+            if (slashIdx == -1) throw new DukeException("Format");
+
+            String taskTitle = taskDescription.substring(0, slashIdx);
+            String location = taskDescription.substring(slashIdx + 4);
+            t = new Event(taskTitle, location);
+        } else {
+            t = null;
+            throw new DukeException("Unrecognised");
+        }
+
+        if (t != null) {
+            list.add(t);
+            System.out.println("This task has been added successfully:");
+            System.out.println(t);
+            System.out.println("Now you have " + list.size() + " tasks in the list");
+        }
+    }
+
+    private static void deleteTask(ArrayList<Task> list, String userInput) throws DukeException {
+        if (userInput.equals("delete")) throw new DukeException(userInput);
+        int taskNumber = Character.getNumericValue(userInput.charAt(7));
+        if (taskNumber > list.size()) throw new DukeException("List size");
+        Task t = list.get(taskNumber - 1);
+        list.remove(taskNumber - 1);
+        System.out.println("This task has successfully been deleted!");
+        System.out.println(t);
+        System.out.println("You have " + list.size() + " tasks remaining.");
+    }
+
+    private static void completeTask(ArrayList<Task> list, String userInput) throws DukeException {
+        if (userInput.equals("done")) throw new DukeException(userInput);
+        int taskNumber = Character.getNumericValue(userInput.charAt(5));
+        if (taskNumber > list.size()) throw new DukeException("List size");
+        Task t = list.get(taskNumber - 1);
+        t.markAsDone();
+        System.out.println("This task is marked as completed!");
+        System.out.println(t);
+    }
+
+    private static void printList(ArrayList<Task> list) {
+        System.out.println("This is your list of tasks");
+        for (int i = 0; i < list.size(); i++) {
+            Task t = list.get(i);
+            System.out.println((i+1) + "." + t);
         }
     }
 }
