b798e57ee406bb49255c522896cd4aef2fe69636
umaikaze <zmj10086@gmail.com>
2020-01-28 23:59:12 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+    static List<String> list = new ArrayList<>(100);
+    static int longest = 0;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String logo = " _____     _                          \n"
@@ -12,7 +17,16 @@ public class Duke {
         System.out.println("hewwo fwom\n" + logo);
         String line = sc.nextLine();
         while (!line.equals("bye")) {
-            System.out.print(formatReply(line));
+            if (line.equals("list")) {
+                System.out.print(formatList());
+            } else {
+                if (line.length() > longest) {
+                    longest = line.length() + 3;
+                }
+                list.add(line);
+                System.out.print(formatReply("added: " + line));
+            }
+
             line = sc.nextLine();
         }
         System.out.print(formatReply("Bye. Hope to see you again soon >w<"));
@@ -30,6 +44,34 @@ public class Duke {
             sb.append("*");
         }
         sb.append("\n");
+        return sb.toString();
+    }
+
+    private static String formatList() {
+        StringBuilder sb = new StringBuilder("\t");
+        if (list.size() == 0) {
+            for (int i = 0; i < 17; i++) {
+                sb.append("*");
+            }
+            sb.append("\n\t");
+            sb.append("list is empty qwq\n\t");
+            for (int i = 0; i < 17; i++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        } else {
+            for (int i = 0; i < longest; i++) {
+                sb.append("*");
+            }
+            sb.append("\n\t");
+            for (int i = 1; i <= list.size(); i++) {
+                sb.append(i + ". " + list.get(i - 1) + "\n\t");
+            }
+            for (int i = 0; i < longest; i++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
         return sb.toString();
     }
 }
