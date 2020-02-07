6670b407853c760b4081e03a193c92e166f360ba
Alaukik <alaukik.pant@u.yale-nus.edu.sg>
2020-02-07 21:17:35 +0800

    A-CodeQuality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,22 +5,25 @@ import java.util.Arrays;
 
 
 public class Duke {
-
-    //Class variables
+    /**
+     * Keeps track of Class variable taskList.
+     */
     private ArrayList<Task> taskList;
 
-
-    //Constructor
+    /**
+     * Constructor
+     */
     public Duke() {
         this.taskList = new ArrayList<Task>();
-
     }
 
     public void printStraightLine(){
         System.out.println("___________________________________________________________________________\n");
     }
 
-    //Logo generator
+    /**
+     * Generates Logo.
+     */
     public void printLogo(){
         for (int i = 0; i < 5; i++){
             String addOne = "*";
@@ -38,26 +41,31 @@ public class Duke {
                 addSix += "*";
             }
             System.out.println(addOne + addTwo + addThree + addFour + addFive + addSix +
-                                    addOne + addTwo + addThree + addFour + addFive + addSix);
-
+                                    addOne + addTwo + addThree + addFour + addFive + addSix +
+                                        addOne + addTwo + addThree + addFour + addFive + addSix +
+                                            addOne + addTwo + addThree + addFour + addFive + addSix);
         }
     }
 
-    //Print all available tasks
+    /**
+     * Print all available tasks.
+     */
     public void printTaskList(ArrayList<Task> taskList){
-        int i = 1;
+        int taskCounter = 1;
         for (Task task : taskList){
             String description = task.getDescription();
             String statusIcon = task.getStatusIcon();
             String typeIcon = task.getTypeIcon();
-            System.out.println(i + ". "  + typeIcon +  " [" +  statusIcon + "] " +  description);
-            i+= 1;
+            System.out.println(taskCounter + ". "  + typeIcon +  " [" +  statusIcon + "] " +  description);
+            taskCounter+= 1;
         }
     }
 
 
-    // Check if the given task is valid
-    public Boolean checkIfValidTask (String type){
+    /**
+     * Checks if the given task is valid
+      */
+    public Boolean checkIfValidTask(String type){
         String DONE = "done";
         String DEADLINE = "deadline";
         String TODO = "todo";
@@ -72,16 +80,33 @@ public class Duke {
     }
 
 
-    //Mark as done
-    public Boolean markDone(String line, ArrayList<Task> taskList) {
+    /**
+     * Returns a boolean if a given task is marked as done
+     *
+     * @param line  Line that represents the task that is supposed to marked as done.
+     * @param taskList TaskList of all available tasks.
+     * @return isDone Is the task marked as Done.
+     */
+    public Boolean hasDone(String line, ArrayList<Task> taskList) {
         String[] words = line.split(" ");
         int LENG_LIST = words.length;
+        Boolean isDone;
+        //Error check, to make sure you are given a valid done statement
         if (LENG_LIST == 1){
             if (words[0].toUpperCase().equals("DONE")) {
                 System.out.println("Please specify (eg: done 2) or just add a new one");
-                return true;
+                isDone = true;
+                return isDone;
             }
         }
+        isDone = markValidTaskAsDone(taskList, words);
+        return isDone;
+    }
+
+    /**
+     * Returns a boolean that signifies if the task that was supposed to be marked exists and if it is marked.
+     */
+    private Boolean markValidTaskAsDone(ArrayList<Task> taskList, String[] words) {
         Boolean isSet= false;
         if (words[0].toUpperCase().equals("DONE")) {
             int IDX_WORDS = Integer.parseInt(words[1]) - 1;
@@ -102,8 +127,12 @@ public class Duke {
         return isSet;
     }
 
-
-    // Returns the description in the required format
+    /**
+     * Returns the description of the task in the required format
+     * @param line Line that represents the task that is supposed to marked as done.
+     * @type type Type of subclass of task.
+     * @return description Description of the task in the required format.
+     */
     public String returnStringToAdd(String line, String type) {
         if (type.equals("todo") ){
             String[] arrOfStr = line.split(type);
@@ -126,13 +155,17 @@ public class Duke {
             toReturn[0] = (todo[1].substring(1, todo[1].length()));       // task
 
             toReturn[1] = arrOfStr[1].substring(1, arrOfStr[1].length()); // date
-            return toReturn[0] + "(" + preposition + toReturn[1] + ")";
+            String description = toReturn[0] + "(" + preposition + toReturn[1] + ")";
+            return description;
         }
     }
 
-
-    //Add a task
-    public void addTask(String line, ArrayList<Task> taskList) {
+    /**
+     * Reads what type of task if given and calls the required method accordingly.
+     * @param line Line that represents the task that is supposed to marked as done.
+     * @param taskList Tasklist of all available tasks.
+     */
+    public void readCommands(String line, ArrayList<Task> taskList) {
         String ogString = line;
         while (!ogString.toUpperCase().equals("BYE")) {
             String[] words = ogString.split(" ");
@@ -142,37 +175,7 @@ public class Duke {
                 System.out.println("Please specify (eg: done, todo, deadline, list, event)");
             }
             else {
-                if (markDone(ogString, taskList)){
-                    printStraightLine();
-                } else if (ogString.toUpperCase().equals("LIST")) {
-                    printStraightLine();
-                    this.printTaskList(taskList);
-                    printStraightLine();
-                } else {
-                    printStraightLine();
-                    String todoOrDeadlineOrEvent = returnStringToAdd(ogString, eventType);
-                    if (eventType.equals("event") || eventType.equals("deadline")) {
-                        if (eventType.equals("event")) {
-                            Event t = new Event(todoOrDeadlineOrEvent);
-                            taskList.add(t);
-                        } else {
-                            Deadline t = new Deadline(todoOrDeadlineOrEvent);
-                            taskList.add(t);
-                        }
-                    } else {
-                        Todo e = new Todo(todoOrDeadlineOrEvent);
-                        taskList.add(e);
-                    }
-
-                    Task t = taskList.get(taskList.size() - 1);
-                    String description = t.getDescription();
-                    String statusIcon = t.getStatusIcon();
-                    String typeIcon = t.getTypeIcon();
-                    System.out.println("Got it. I have added this task: \n" + todoOrDeadlineOrEvent);
-                    System.out.println(typeIcon +  " [" +  statusIcon + "] " +  description + "\n");
-                    System.out.println("Now you have " + taskList.size() +  " items in the list \n");
-                    printStraightLine();
-                }
+                performTasks(taskList, ogString, eventType);
             }
             Scanner scanner = new Scanner(System.in);
             ogString = scanner.nextLine();
@@ -181,8 +184,49 @@ public class Duke {
         printStraightLine();
     }
 
+    /**
+     * Performs the requested task
+     * @param taskList TaskList of all available tasks.
+     * @param ogString Original string that is inputted by the user using the command line.
+     * @param eventType Event Type, meaning the nature of the command (eg. LIST, DEADLINE).
+     */
+    public void performTasks(ArrayList<Task> taskList, String ogString, String eventType) {
+        if (hasDone(ogString, taskList)){
+            printStraightLine();
+        } else if (ogString.toUpperCase().equals("LIST")) {
+            printStraightLine();
+            this.printTaskList(taskList);
+            printStraightLine();
+        } else {
+            printStraightLine();
+            String todoOrDeadlineOrEvent = returnStringToAdd(ogString, eventType);
+            if (eventType.equals("event") || eventType.equals("deadline")) {
+                if (eventType.equals("event")) {
+                    Event t = new Event(todoOrDeadlineOrEvent);
+                    taskList.add(t);
+                } else {
+                    Deadline t = new Deadline(todoOrDeadlineOrEvent);
+                    taskList.add(t);
+                }
+            } else {
+                Todo e = new Todo(todoOrDeadlineOrEvent);
+                taskList.add(e);
+            }
+            Task t = taskList.get(taskList.size() - 1);
+            String description = t.getDescription();
+            String statusIcon = t.getStatusIcon();
+            String typeIcon = t.getTypeIcon();
+            System.out.println("Got it. I have added this task: \n" + todoOrDeadlineOrEvent);
+            System.out.println(typeIcon +  " [" +  statusIcon + "] " +  description + "\n");
+            System.out.println("Now you have " + taskList.size() +  " items in the list \n");
+            printStraightLine();
+        }
+    }
 
-    //Get things started
+
+    /**
+     * Gets the program statted by adding an initial task or taking the initial command
+     */
     public void startThingsOff(){
         printLogo();
         Scanner scanner = new Scanner(System.in);
@@ -192,14 +236,13 @@ public class Duke {
         System.out.println("Lets plan your day buddy! Things are looking good.\n");
         printStraightLine();
         line = scanner.nextLine();
-        addTask(line, taskList);
+        readCommands(line, taskList);
         }
 
 
     public static void main(String[] args) {
         Duke hiroshiNagai = new Duke();
         hiroshiNagai.startThingsOff();
-
     }
 
 }
