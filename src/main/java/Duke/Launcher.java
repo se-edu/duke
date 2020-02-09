5fa42f5a8a21ce64be6cf9b5b8d0c576e6befc5d
Deunitato <charlotte.limwt@gmail.com>
2020-02-09 21:12:12 +0800

    Attempted GUI

---
+++ src/main/java/Duke/Launcher.java
@@ -0,0 +1,14 @@
+package Duke;
+
+import Duke.Main;
+import Duke.*;
+import javafx.application.Application;
+
+/**
+ * A launcher class to workaround classpath issues.
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(Main.class, args);
+    }
+}
---
+++ src/main/java/Duke/Main.java
@@ -0,0 +1,37 @@
+package Duke;
+import Duke.Main;
+import Duke.MainWindow;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * A GUI for Duke.Duke using FXML.
+ */
+public class Main extends Application {
+
+    private Duke duke = new Duke();
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            System.out.println("lolcancer");
+            System.out.println(Main.class.getResource("/view/MainWindow.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            fxmlLoader.<MainWindow>getController().setDuke(duke);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
