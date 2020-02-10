15a1bf4db1d802beee0fac6df397dda1640f5586
vigneshbhuvan-nus <e0323321@u.nus.edu>
2020-02-10 12:06:15 +0800

    Duke- Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,48 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
+        boolean end = false;
+
+        String opening_message = "____________________________________________________________\n" +
+                "     Hello! I'm Duke\n" +
+                "     What can I do for you?\n" +
+                "____________________________________________________________\n";
+        System.out.println(opening_message);
+
+        while(!end) {
+            String next_cmd = sc.nextLine();
+            if (next_cmd.equals("bye")) {
+                end = true;
+                System.out.println("    ____________________________________________________________\n" +
+                        "     Bye. Hope to see you again soon!\n" +
+                        "    ____________________________________________________________");
+            }
+            else if (next_cmd.equals("list")) {
+                String list_of_items = "";
+                for(int i = 0; i < list.size(); i++) {
+                    list_of_items += list.get(i) + "\n";
+                }
+                System.out.println("    ____________________________________________________________\n" +
+                        "     " + list_of_items + "\n" +
+                        "    ____________________________________________________________");
+            }
+            else if (next_cmd.equals("blah")) {
+                System.out.println("    ____________________________________________________________\n" +
+                        "     blah\n" +
+                        "    ____________________________________________________________");
+
+            }
+            else {
+                list.add(next_cmd);
+                System.out.println("    ____________________________________________________________\n" +
+                        "     " + next_cmd + "\n" +
+                        "    ____________________________________________________________");
+            }
+        }
+        sc.close();
     }
 }
