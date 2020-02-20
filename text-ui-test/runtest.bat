e821ecc77da5cc55190dd93de78939d6df7b2ff1
Jeffry Lum <22460123+j-lum@users.noreply.github.com>
2020-02-20 23:35:44 +0800

    Improve text UI testing
    Include the generation of an executable jar in the shell script.
    Change the name of the shell scripts to runtest to match iP.
    Add tests on Windows and macOS for coverage.

---
+++ text-ui-test/runtest.bat
@@ -0,0 +1,19 @@
+@echo off
+setlocal enableextensions
+pushd %~dp0
+
+cd ..
+call gradlew shadowJar
+
+cd build\libs
+for /f "tokens=*" %%a in (
+    'dir /b *.jar'
+) do (
+    set jarloc=%%a
+)
+
+java -jar %jarloc% < ..\..\text-ui-test\input.txt > ..\..\text-ui-test\ACTUAL.TXT
+
+cd ..\..\text-ui-test
+
+FC ACTUAL.TXT EXPECTED.TXT >NUL && ECHO Test passed! || Echo Test failed!
--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -3,9 +3,14 @@
 # change to script directory
 cd "${0%/*}"
 
+cd ..
+./gradlew shadowJar
+
+cd text-ui-test
+
 java  -jar $(find ../build/libs/ -mindepth 1 -print -quit) < input.txt > ACTUAL.TXT
 
-diff ACTUAL.TXT EXPECTED.TXT
+diff EXPECTED.TXT ACTUAL.TXT
 if [ $? -eq 0 ]
 then
     echo "Test passed!"
