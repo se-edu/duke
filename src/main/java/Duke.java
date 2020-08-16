259e3fb60dcf25addd67a867ffb1f08047f2edd5
Tra Quang Minh Thong <tqmthong@gmail.com>
2020-08-16 17:13:16 +0800

    Add level-0 increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,16 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        String Horizontal = "______________________________________\n";
+        String HelloGreet =
+                " Hello! I'm Duke\n"
+                        +" What can I do for you?\n";
+        String GoodBye = " Bye. Hope to see you again soon!\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println(Horizontal);
+        System.out.println(HelloGreet);
+        System.out.println(Horizontal);
+        System.out.println(GoodBye);
+        System.out.println(Horizontal);
     }
 }
