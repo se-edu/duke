31ed09ead446abf648e46892e330d770fe27bfbe
snowbanana12345 <50855780+snowbanana12345@users.noreply.github.com>
2020-08-16 22:28:45 +0800

    Level-0 implemented

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,27 @@
 public class Duke {
     public static void main(String[] args) {
+        /*
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("CAN YOU SEE THE CHANGE?");
+        System.out.println("CAN YOU SEE THE CHANGE?");*/
+        printHorizontalLine(50);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println();
+        printHorizontalLine(50);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println();
+        printHorizontalLine(50);
+    }
+
+    public static void printHorizontalLine(int length){
+        for (int i = 0; i < length; i++){
+            System.out.print("_");
+        }
+        System.out.print("\n");
     }
 }
