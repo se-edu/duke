10bd5f2416bd6eb801a80eb0744e97bc60389e77
Greek <ygreek@gmail.com>
2020-08-20 20:26:13 +0800

    Level-0 completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String initial = "____________________________________________________________\n"
+                + "Hello! I'm Duke\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________\n";
+        System.out.println(initial);
+        for(String word : args) {
+            if(word.equals("bye")) {
+                System.out.println(
+                    "____________________________________________________________\n"
+                    + "Bye. Hope to see you again soon!\n"
+                    + "____________________________________________________________\n"
+                );
+                break;
+            } else {
+                System.out.println(
+                    "____________________________________________________________\n"
+                    + word + " \n"
+                    + "____________________________________________________________\n"
+                );
+            }
+        }
     }
 }
