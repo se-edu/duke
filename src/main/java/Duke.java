87541a2a0c76b4fe1b2b3e383a93f1bebc4f4895
jessicazhang617 <47000098+jessicazhang617@users.noreply.github.com>
2020-08-21 17:49:23 +0800

    Added basic greetings

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,6 +6,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        //test comment
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
