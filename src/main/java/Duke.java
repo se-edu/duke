606d3d708c2f9ccb43c762146ff53259de28c2b8
tys <tanyushing@outlook.sg>
2020-08-22 11:45:42 +0800

    Signed-off-by: tys <tanyushing@outlook.sg>

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -20,7 +20,6 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Duke");
         System.out.println(" What can I do for you?");
