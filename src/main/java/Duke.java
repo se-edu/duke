dcdfb9e75fe35c8dae77ebfad8b19aed7f347c7c
tys <tanyushing@outlook.sg>
2020-08-22 15:28:41 +0800

    Signed-off-by: tys <tanyushing@outlook.sg>

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,40 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
+    public static class Task{
+        private String data;
+        private boolean isDone;
+        private int taskNumber;
+        private static int numberOfTasks = 0;
+
+        public Task(String input1, boolean input2){
+            data = input1;
+            isDone = input2;
+            numberOfTasks++;
+            taskNumber = numberOfTasks;
+        }
+
+        public void setIsDone(boolean input1){
+            isDone = input1;
+        }
+
+        public boolean getIsDone(){
+            return isDone;
+        }
+
+        public String getData(){
+            return data;
+        }
+
+        public void printTask(){
+            if(isDone == true){
+                System.out.printf("%d.[✓] %s", taskNumber, data);
+            }else{
+                System.out.printf("%d.[✗] %s", taskNumber, data);
+            }
+        }
+    }
+
     public static void printMessagae(String message){
         System.out.println("____________________________________________________________");
         if(message.equals("bye")){
@@ -15,11 +49,23 @@ public class Duke {
         System.out.println("____________________________________________________________");
     }
 
-    public static void printList(String[] list, int listLength){
+    public static void printList(Task[] list, int listLength){
         System.out.println("____________________________________________________________");
+        System.out.println(" Here are the tasks in your list");
         for(int i = 0; i < listLength; i++){
-            System.out.printf("%d. %s\n", i, list[i]);
+            System.out.print(" ");
+            list[i].printTask();
+            System.out.println();
         }
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void printDoneStatement(Task taskObject){
+        System.out.println("____________________________________________________________");
+        System.out.print(" Nice! I've marked this task as done: ");
+        System.out.println();
+        System.out.printf(" [✓] %s", taskObject.getData());
+        System.out.println();
         System.out.println("____________________________________________________________");
     }
 
@@ -35,7 +81,7 @@ public class Duke {
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
 
-        String[] listInputs = new String[100];
+        Task[] listInputs = new Task[100];
 
         String input = "";
         Scanner in = new Scanner(System.in);
@@ -45,19 +91,23 @@ public class Duke {
 
         while(isRunning == true){
             input = in.nextLine();
-            switch(input){
-            case "bye":
+            if(input.equals("bye")) {
                 printMessagae(input);
                 isRunning = false;
-                break;
-            case "list":
+            }else if (input.equals("list")){
                 printList(listInputs, listPosition);
-                break;
-            default:
+            }else if("done".equals(input.substring(0,4))) {
+                int taskNumber = Integer.parseInt(input.substring(5, input.length())) - 1;
+                if(taskNumber < listPosition && taskNumber >= 0){
+                    listInputs[taskNumber].setIsDone(true);
+                    printDoneStatement(listInputs[taskNumber]);
+                }else{
+                    printMessagae("Invalid task number");
+                }
+            }else{
                 printMessagae("added: " + input);
-                listInputs[listPosition] = input;
+                listInputs[listPosition] = new Task(input, false);
                 listPosition++;
-                break;
             }
         }
     }
