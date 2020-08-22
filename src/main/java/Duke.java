f75b7674c168e1005701ab36c314ff8eb5ea190d
tys <tanyushing@outlook.sg>
2020-08-22 11:41:02 +0800

    Signed-off-by: tys <tanyushing@outlook.sg>

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,18 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static void printMessagae(String message){
+        System.out.println("____________________________________________________________");
+        if(message.equals("bye")){
+            System.out.println(" Bye. Hope to see you again soon!");
+        }else{
+            System.out.printf(" %s", message);
+            System.out.println();
+        }
+
+        System.out.println("____________________________________________________________");
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -11,7 +25,16 @@ public class Duke {
         System.out.println(" Hello! I'm Duke");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        String input = "";
+        Scanner in = new Scanner(System.in);
+
+        while(true){
+            input = in.nextLine();
+            printMessagae(input);
+            if(input.equals("bye")){
+                break;
+            }
+        }
     }
 }
