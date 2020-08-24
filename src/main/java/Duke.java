bcd1d7f7f6924e90d23816b7d6c43bec3e33b7cf
Alvin Tan De Jun <>
2020-08-25 00:14:01 +0800

    Level-3: Add Task and TaskManager class and ability to mark task as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,23 +3,38 @@ import java.util.ArrayList;
 
 public class Duke {
     private static final String horizontalLine = "____________________________________________________________";
-    private static ArrayList<String> taskArrayList = new ArrayList<String>();
+    private static TaskManager taskManager = new TaskManager();
+
+    public static void printMessage(String message) {
+        printWithIndentation(horizontalLine);
+        printWithIndentation(message);
+        printWithIndentation(horizontalLine);
+    }
+
+    public static void printIntroduction() {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println(logo);
+        printMessage("Hello! I'm Duke\n" + "\tWhat can I do for you?");
+    }
+
+    public static void printGoodBye() {
+        printMessage("Bye. Hope to see you again soon!");
+    }
 
     public static void printWithIndentation(String string) {
         System.out.println("\t" + string);
     }
 
-    public static void addToTaskArrayList(String description) {
-        taskArrayList.add(description);
+    public static void printTasks() {
         printWithIndentation(horizontalLine);
-        printWithIndentation("added: " + description);
-        printWithIndentation(horizontalLine);
-    }
-
-    public static void printTaskArrayList() {
-        printWithIndentation(horizontalLine);
+        ArrayList<Task> taskArrayList = taskManager.getTasksList();
         for (int i = 0; i < taskArrayList.size(); i++) {
-            printWithIndentation((i + 1) + ". " + taskArrayList.get(i));
+            printWithIndentation((i + 1) + ". [" + taskArrayList.get(i).getStatusIcon() + "] "
+                + taskArrayList.get(i).description);
         }
         printWithIndentation(horizontalLine);
     }
@@ -27,27 +42,38 @@ public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        printWithIndentation(horizontalLine);
-        printWithIndentation("Hello! I'm Duke");
-        printWithIndentation("What can I do for you?");
-        printWithIndentation(horizontalLine);
+        printIntroduction();
 
         String input = sc.nextLine();
-
         while (true) {
-            if (input.equals("bye")) {
-                break;
-            } else if (input.equals("list")) {
-                printTaskArrayList();
+            String[] inputSplit = input.split(" ");
+            if (inputSplit.length == 2 && inputSplit[0].toLowerCase().equals("done")) {
+                try {
+                    int indexOfTask = Integer.parseInt(inputSplit[1]) - 1;
+                    if (taskManager.markTaskAsDone(indexOfTask)) {
+                        Task taskMarkedAsDone = taskManager.getTasksList().get(indexOfTask);
+                        printMessage("Nice! I've marked this task as done:\n" +
+                        "\t[" + taskMarkedAsDone.getStatusIcon() + "] " + taskMarkedAsDone.description);
+                    } else {
+                        printMessage("Index out of bounds!");
+                    }
+                } catch (NumberFormatException e) {
+                    printMessage("Invalid index of task!");
+                }
             } else {
-                addToTaskArrayList(input);
+                if (input.toLowerCase().trim().equals("bye")) {
+                    break;
+                } else if (input.toLowerCase().trim().equals("list")) {
+                    printTasks();
+                } else {
+                    taskManager.addTask(input);
+                    printMessage("added: " + input);
+                }
             }
 
             input = sc.nextLine();
         }
 
-        printWithIndentation(horizontalLine);
-        printWithIndentation("Bye. Hope to see you again soon!");
-        printWithIndentation(horizontalLine);
+        printGoodBye();
     }
 }
---
+++ src/main/java/TaskManager.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class TaskManager {
+    private ArrayList<Task> tasksList = new ArrayList<Task>();
+
+    public ArrayList<Task> getTasksList() {
+        return tasksList;
+    }
+
+    public void addTask(String description) {
+        tasksList.add(new Task(description));
+    }
+
+    public boolean markTaskAsDone(int indexOfTask) {
+        try {
+            Task task = tasksList.get(indexOfTask);
+            task.markAsDone();
+            return true;
+        } catch (IndexOutOfBoundsException e) {
+            return false;
+        }
+    }}
