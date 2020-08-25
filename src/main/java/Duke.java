aa1cbdee8f0a4fef75b12d48e8779d82796d45f5
Brandon Quek <brandonqxy@outlook.com>
2020-08-25 21:36:05 +0800

    Add ability to store and display text by user

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,14 +9,26 @@ public class Duke {
                 + " ____________________________________________________________\n";
         System.out.println(greet);
         String line;
+        String[] storeTask = new String[100];
+        int numOfTasks = 0;
         Scanner in = new Scanner(System.in);
 
         line = in.nextLine();
         while (!(line.equals("bye"))) {
+            storeTask[numOfTasks] = line;
+            numOfTasks++;
             System.out.println(" ____________________________________________________________");
-            System.out.println("  " + line);
+            System.out.println("  added: " + line);
             System.out.println(" ____________________________________________________________");
             line = in.nextLine();
+            if (line.equals("list")) {
+                System.out.println(" ____________________________________________________________");
+                for (int i = 0; i < numOfTasks; i++) {
+                    System.out.println(" " + (i + 1) + ". " + storeTask[i]);
+                }
+                System.out.println(" ____________________________________________________________");
+                line = in.nextLine();
+            }
         }
         System.out.println(" ____________________________________________________________");
         System.out.println("  Bye. Hope to see you again soon!");
