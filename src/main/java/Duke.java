b653b938ba350743e9c06ef8bb76f527f893faf3
Brandon Quek <brandonqxy@outlook.com>
2020-08-25 21:15:54 +0800

    Add command echoing feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String greet = " ____________________________________________________________\n"
                 + "  Hello! I'm Duke\n"
                 + "  What can I do for you?\n"
                 + "\n"
-                + " ____________________________________________________________\n"
-                + "  Bye. Hope to see you again soon!\n"
-                + " ____________________________________________________________";
+                + " ____________________________________________________________\n";
         System.out.println(greet);
+        String line;
+        Scanner in = new Scanner(System.in);
+
+        line = in.nextLine();
+        while (!(line.equals("bye"))) {
+            System.out.println(" ____________________________________________________________");
+            System.out.println("  " + line);
+            System.out.println(" ____________________________________________________________");
+            line = in.nextLine();
+        }
+        System.out.println(" ____________________________________________________________");
+        System.out.println("  Bye. Hope to see you again soon!");
+        System.out.println(" ____________________________________________________________");
     }
 }
