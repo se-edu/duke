d230588c80e611f8a795115182c010fb667b2435
abnermtj <abnermtj@live.com.sg>
2020-08-26 12:16:13 +0800

    add listing

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,15 +2,36 @@ import java.util.Scanner;
 
 public class Duke {
     static Scanner scan = new Scanner(System.in);
+    static int wordListIndex = 0;
+    static String wordList[] = new String[100];
 
-    public static String echoInput(){
-        String input = scan.nextLine();
-        System.out.println("---------------------------------------");
-        System.out.println(input);
+    public static String addInput(String input){
+        System.out.println("added: " + input);
         System.out.println("---------------------------------------");
 
+        wordList[wordListIndex] = input;
+        wordListIndex++;
         return input;
+
     }
+
+    public static void listWordList(){
+        int i = 1;
+        for (String word : wordList){
+            if (word == null) {
+                if (i == 1){
+                    System.out.println("---------------------------------------");
+                }
+                break;
+            }
+            else {
+                System.out.println(String.valueOf(i) + ". " + word);
+                i++;
+            }
+        }
+        System.out.println("---------------------------------------");
+    }
+
     public static void main(String[] args) {
         System.out.println("---------------------------------------");
         System.out.println("Hello! I'm Duke");
@@ -19,9 +40,20 @@ public class Duke {
 
         boolean isRunning = true;
         while(isRunning){
-            String input = echoInput();
-            if (input.equals("bye")){
-                isRunning = false;
+            String input = scan.nextLine();
+
+            switch (input){
+                case "bye" : {
+                    isRunning = false;
+                    break;
+                }
+                case "list" : {
+                    listWordList();
+                    break;
+                }
+                default:{
+                    addInput(input);
+                }
             }
         }
 
