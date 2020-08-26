9001c6d672dcc9f5a4fe00552803b990f82d2495
abnermtj <abnermtj@live.com.sg>
2020-08-26 15:46:11 +0800

    add finishing tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,36 +1,8 @@
 import java.util.Scanner;
-
 public class Duke {
     static Scanner scan = new Scanner(System.in);
-    static int wordListIndex = 0;
-    static String wordList[] = new String[100];
-
-    public static String addInput(String input){
-        System.out.println("added: " + input);
-        System.out.println("---------------------------------------");
-
-        wordList[wordListIndex] = input;
-        wordListIndex++;
-        return input;
-
-    }
-
-    public static void listWordList(){
-        int i = 1;
-        for (String word : wordList){
-            if (word == null) {
-                if (i == 1){
-                    System.out.println("---------------------------------------");
-                }
-                break;
-            }
-            else {
-                System.out.println(String.valueOf(i) + ". " + word);
-                i++;
-            }
-        }
-        System.out.println("---------------------------------------");
-    }
+    static int taskListIndex = 0;
+    static Task taskList[] = new Task[100];
 
     public static void main(String[] args) {
         System.out.println("---------------------------------------");
@@ -42,22 +14,70 @@ public class Duke {
         while(isRunning){
             String input = scan.nextLine();
 
-            switch (input){
-                case "bye" : {
-                    isRunning = false;
-                    break;
+            if  (input.equals("bye")) {
+                isRunning = false;
+            } else if (input.equals("list")){
+                listTaskList();
+            } else if (input.matches("done.*")) {
+                input = input.substring(5); // removes the "done " part of the string
+                Task task = finishTask(input);
+                if (task != null){
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.print("\t");
+                    printTaskStatus(task);
+                    System.out.println("---------------------------------------");
                 }
-                case "list" : {
-                    listWordList();
-                    break;
-                }
-                default:{
-                    addInput(input);
-                }
+            } else {
+                addInput(input);
             }
         }
 
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("---------------------------------------");
+    }
+
+    public static void listTaskList(){
+        int i = 1;
+        for (Task task : taskList){
+            if (task == null) {
+                if (i == 1){
+                    System.out.println("---------------------------------------");
+                }
+                break;
+            }
+            else {
+                System.out.print(String.valueOf(i) + ".");
+                printTaskStatus(task);
+                i++;
+            }
+        }
+        System.out.println("---------------------------------------");
+    }
+
+    public static void printTaskStatus(Task task){
+        System.out.println("[" + task.getStatusIcon() +"]" +
+                " " + task.description);
+    }
+
+    public static Task finishTask(String taskName){
+        for (Task task : taskList){
+            if (task != null && task.description.equals(taskName)) {
+                task.isDone = true;
+                return task;
+            }
+        }
+
+        return null;
+    }
+
+    public static String addInput(String input){
+        System.out.println("added: " + input);
+        System.out.println("---------------------------------------");
+
+        Task newTask =  new Task(input);
+        taskList[taskListIndex] = newTask;
+        taskListIndex++;
+
+        return input;
     }
 }
