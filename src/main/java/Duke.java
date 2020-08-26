7f029472f548aa29d47d63ee11e4fc87328620df
gua-guargia <georgiazhang221@gmail.com>
2020-08-27 04:30:52 +0800

    Add Level-1 Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        boolean exit = false;
+        String line;
+        Scanner in = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -7,6 +12,20 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         greet();
+        while(!exit) {
+            line = in.nextLine();
+            //System.out.println(line);
+            if(line.equals("list")) {
+                list();
+            }
+            else if(line.equals("bye")) {
+                exit = true;
+            }
+            else {
+                echo(line);
+            }
+        }
+        exit();
     }
 
     public static void greet() {
@@ -14,7 +33,21 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
+    }
+
+    public static void echo(String message) {
+        System.out.println("____________________________________________________________");
+        System.out.println(message);
+        System.out.println("____________________________________________________________");}
+
+    public static void exit() {
+        System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
+
+    public static void list() {
+        System.out.println("____________________________________________________________");
+        System.out.println("list");
+        System.out.println("____________________________________________________________");}
 }
