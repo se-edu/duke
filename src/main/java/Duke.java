baded1d41bd4adbec48ed1e249986c43759d2649
joelngyx <joelngyx@gmail.com>
2020-08-27 21:27:20 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -8,8 +10,26 @@ public class Duke {
                 + "____________________________________________________________\n"
                 + "Hello! I'm Duke\n"
                 + "What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + "Bye. Hope to see you again soon!";
+                + "____________________________________________________________\n";
         System.out.println("Hello from\n" + logo);
+        getInput();
+    }
+    public static void getInput() {
+        Scanner in = new Scanner(System.in);
+        String byeMessage = ""
+                + "____________________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
+        String input = in.nextLine();
+        while((input.equals("bye") != true) && (input.equals("Bye") != true)){
+            System.out.println(""
+                + "____________________________________________________________\n"
+                + input
+                + "\n"
+                + "____________________________________________________________\n"
+            );
+            input = in.nextLine();
+        }
+        System.out.println(byeMessage);
     }
 }
