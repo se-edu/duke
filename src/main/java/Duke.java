ff0868b17da02d8c06baaf2000463413f58db14a
jessicazhang617 <47000098+jessicazhang617@users.noreply.github.com>
2020-08-28 00:17:12 +0800

    Added Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -7,6 +9,24 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
+        Echo();
+
+    }
+
+    public static void Echo() {
+        Scanner in = new Scanner(System.in);
+        while (true){
+        String line = in.nextLine();
+        if (line.contains("bye") || line.equalsIgnoreCase("bye")) {
+            Bye();
+            break;
+        }else {
+            System.out.println(line);
+        }
+        }
+
+    }
+    public static void Bye(){
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
