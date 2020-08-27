facd3383a67590d0725db9c085c0290fbccb2f35
jessicazhang617 <47000098+jessicazhang617@users.noreply.github.com>
2020-08-28 00:34:03 +0800

    Add List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,19 +9,28 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
-        Echo();
+        List();
 
     }
 
-    public static void Echo() {
+    public static void List() {
+        String list[] = new String[100];
         Scanner in = new Scanner(System.in);
+        int number = 1;
+
         while (true){
         String line = in.nextLine();
         if (line.contains("bye") || line.equalsIgnoreCase("bye")) {
             Bye();
             break;
-        }else {
-            System.out.println(line);
+        }else if(line.equalsIgnoreCase("list")){
+            for (int i=0;i<number-1;i++){
+                System.out.println(i+1 + ". " + list[i]);
+            }
+        }else{
+            list[number-1] = line;
+            System.out.println("added " + line);
+            number++;
         }
         }
 
@@ -29,4 +38,5 @@ public class Duke {
     public static void Bye(){
         System.out.println("Bye. Hope to see you again soon!");
     }
+
 }
