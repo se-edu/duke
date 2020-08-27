c702c9a947a98423806b935fb956b2b776b0b46c
joelngyx <joelngyx@gmail.com>
2020-08-27 22:51:19 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 
 public class Duke {
+    public static final String BYE_MESSAGE = ""
+            + "____________________________________________________________\n"
+            + "Bye. Hope to see you again soon!\n"
+            + "____________________________________________________________\n";
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -12,14 +17,11 @@ public class Duke {
                 + "What can I do for you?\n"
                 + "____________________________________________________________\n";
         System.out.println("Hello from\n" + logo);
-        getInput();
+        addList();
     }
+
     public static void getInput() {
         Scanner in = new Scanner(System.in);
-        String byeMessage = ""
-                + "____________________________________________________________\n"
-                + "Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________\n";
         String input = in.nextLine();
         while((!input.equals("bye")) && (!input.equals("Bye"))){
             System.out.println(""
@@ -30,6 +32,40 @@ public class Duke {
             );
             input = in.nextLine();
         }
-        System.out.println(byeMessage);
+        System.out.println(BYE_MESSAGE);
+    }
+
+    public static void addList(){
+        Scanner in = new Scanner(System.in);
+        String[] list = new String[100];
+        int index = 0;
+
+        String input = in.nextLine();
+        while((!input.equals("bye")) && (!input.equals("Bye"))){
+            if(input.equals("list") || input.equals("List")){
+                int count = 0;
+                System.out.println("____________________________________________________________");
+                while(list[count] != null){
+                    System.out.println(""
+                        + (count + 1) + "." + " "
+                        + list[count]
+                    );
+                    count ++;
+                }
+                System.out.println("____________________________________________________________");
+            }
+            else{
+                list[index] = input;
+                System.out.println(""
+                    + "____________________________________________________________\n"
+                    + "added: " + input
+                    + "\n____________________________________________________________\n"
+                );
+                index++;
+            }
+            input = in.nextLine();
+        }
+        System.out.println(BYE_MESSAGE);
     }
 }
+
