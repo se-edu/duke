5ff2489e2904a86b6195afc7f774e3e54062c446
Lee-Juntong <2310266421@qq.com>
2020-08-28 14:45:28 +0800

    add echo function for the project, exit if user input is "bye"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
        System.out.println("____________________________________________________________");
        System.out.println("Hello! I'm Duke");
        System.out.println("What can I do for you?");
+       Scanner in = new Scanner(System.in);
+
+       String line;
+       while(true){
+           line = in.nextLine();
+           if(line.equals("bye")){
+               break;
+           };
+           System.out.println("____________________________________________________________");
+           System.out.println(line);
+           System.out.println("____________________________________________________________");
+       }
        System.out.println("____________________________________________________________");
        System.out.println("Bye. Hope to see you again soon!");
        System.out.println("____________________________________________________________");
