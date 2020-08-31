47887015926e668b7e951166b05935710c0fc504
jessicazhang617 <47000098+jessicazhang617@users.noreply.github.com>
2020-08-31 15:54:33 +0800

    Update Java conventions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,29 @@
 import java.util.Scanner;
 
+/**
+ * Represents the bot Duke.
+ * Duke can perform tasks based on user input.
+ *
+ * {@value #NUM_OF_TASKS} Number of Tasks.
+ */
 public class Duke {
 
     public static int NUM_OF_TASKS = 0;
 
+    /**
+     * Runs other methods for main.
+     *
+     * @param args User Input.
+     */
     public static void main(String[] args) {
-        Greetings();
-        ListActions();
+        printGreetings();
+        listActions();
     }
-    public static void Greetings(){
+
+    /**
+     * Prints greetings.
+     */
+    public static void printGreetings(){
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -17,55 +32,76 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
     }
-    
-    public static void ListActions() {
-        Task[] List = new Task[100];
+
+    /**
+     * Returns actions for lists based on user input.
+     */
+    public static void listActions() {
+        Task[] list = new Task[100];
         Scanner in = new Scanner(System.in);
         String line;
 
         do {
             line = in.nextLine();
             if (line.contains("done")){
-                UpdateTask(line,List);
+                updateTask(line,list);
             }else if (line.equalsIgnoreCase("bye")){
-                PrintBye();
+                printBye();
                 break;
             }else if(line.equalsIgnoreCase("list")) {
-                PrintList(List);
+                printList(list);
             }else{
-                List[NUM_OF_TASKS] = AddToList(line);
+                list[NUM_OF_TASKS] = addToList(line);
             }
         } while (!line.equalsIgnoreCase("bye"));
 
     }
-
-    public static Task AddToList(String line){
+    /**
+     * Adds new task to the list.
+     *
+     * @param line User input task name.
+     */
+    public static Task addToList(String line){
         Task t = new Task(line);
         System.out.println("added " + line);
         NUM_OF_TASKS++;
         return t;
     }
 
-    public static void PrintList(Task[] List) {
+    /**
+     * Prints task list.
+     *
+     * @param list Task list.
+     */
+    public static void printList(Task[] list) {
         if (NUM_OF_TASKS == 0){
             System.out.println("The list is empty!");
         }else {
             System.out.println("Here are the tasks in your list.");
             for (int i = 0; i < NUM_OF_TASKS; i++) {
-            System.out.println(i+1 + ". " + "[" + List[i].getStatusIcon() + "] " + List[i].description);
+            System.out.println(i+1 + ". " + "[" + list[i].getStatusIcon() + "] " + list[i].description);
             }
         }
     }
 
-    public static void UpdateTask(String line,Task[] List){
+    /**
+     * Marks task as done.
+     *
+     * @param line User Input.
+     * @param list Task list.
+     */
+    public static void updateTask(String line,Task[] list){
         String taskNumString = line.substring(5);
         int taskNumInt = Integer.parseInt(taskNumString);
-        List[taskNumInt-1].markAsDone();
+        list[taskNumInt-1].markAsDone();
         System.out.println("Nice! I've marked this task as done:");
-        System.out.println("["+List[taskNumInt-1].getStatusIcon()+"]"+ List[taskNumInt-1].description);
+        System.out.println("["+list[taskNumInt-1].getStatusIcon()+"]"+ list[taskNumInt-1].description);
     }
 
-    public static void PrintBye(){
+    /**
+     * Prints goodbye.
+     */
+    public static void printBye(){
         System.out.println("Bye. Hope to see you again soon!");
     }
 
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,20 +1,26 @@
+/**
+ * Represents a task.
+ */
 public class Task {
+    /** Description of task.*/
     protected String description;
+    /** Boolean flag of whether task is done.*/
     protected boolean isDone;
 
+    /** Task Constructor.*/
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Retrieves status icon for task.
+     * */
     public String getStatusIcon() {
         return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
     }
 
-    public Task(boolean isDone) {
-        this.isDone = isDone;
-    }
-
+    /** Set boolean isDone as true.*/
     public void markAsDone(){
         isDone = true;
     }
