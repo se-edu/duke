b89cf56aaa3aa1064d1651ddbfa2306e7bf0cfd5
LAPTOP-KBB1KHPN\user <e0563890@u.nus.edu>
2022-01-16 19:10:04 +0800

    Added code for Duke to add and list (Level-2)

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -20,13 +20,25 @@ public class Duke {
         String name = in.nextLine();
         System.out.println("Hello " + name);
 
+        Scanner in = new Scanner(System.in);
+        String name = in.nextLine();
+        System.out.println("Hello " + name);
+
+        String[] commandTracker = new String[100];
+        int index = 0;
         String lineToEcho = in.nextLine();
         while (!lineToEcho.equals("bye")) {
-            System.out.println(lineToEcho);
+            if (lineToEcho.equals("list")) { //if equal list, print all in commandTracker
+                for (int i = 0; i < index; i++) {
+                    //label = index + 1 because array is 0 indexed
+                    System.out.println(String.format("%d . %s",i + 1, commandTracker[i]));
+                }
+            } else { //if not, just add to list
+                commandTracker[index++] = lineToEcho;
+                System.out.println(String.format("added: %s", lineToEcho));
+            }
             lineToEcho = in.nextLine();
         }
         System.out.println(String.format("Bye %s. See you again soon!", name));
-
     }
-
 }
