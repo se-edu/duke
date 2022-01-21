bdacc057c5e080805b61149dfbcd621ce11619fe
LAPTOP-KBB1KHPN\user <e0563890@u.nus.edu>
2022-01-21 11:22:07 +0800

    Fixing checkstyle error from curly bracket in noDateException

--- src/main/java/seedu/duke/exceptions/NoDateException.java
+++ src/main/java/seedu/duke/exceptions/NoDateException.java
@@ -8,7 +8,8 @@ public class NoDateException extends Exception {
     /**
      * parameterless constructor for NoDateException.
      */
-    public NoDateException() { }
+    public NoDateException() {
+    }
 
     /**
      * a constructor for NoDateException.
